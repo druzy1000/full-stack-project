5abc2342ee668c4b4db94b73310cea67ad8972f3
Andrew Reyes <andrew.reyes@ibm.com>
2020-07-10 15:24:34 -0500

    Docker containers for sql and employee service are now communicating. Commented out for debugging purposes.

--- employee-service/src/main/java/com/ibm/fscc/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/ibm/fscc/employeeservice/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +36,28 @@ public class EmployeeController {
     public List<EmployeeEntity> getAllEmployees() {
         return iEmployeeManager.getAllEmployees();
     }
+	
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    EmployeeEntity getEmployee(@PathVariable Long id) {
+    	System.out.println(id);
+        return iEmployeeManager.getEmployeeById(id);
+    }
+    
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    EmployeeEntity createEmployee(@RequestBody EmployeeEntity employeeEntity) {
+    	System.out.println(employeeEntity.toString());
+    	return iEmployeeManager.createEmployee(employeeEntity);
+    }
+    
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.PUT)
+    EmployeeEntity updateEmployee(@PathVariable Long id,  @RequestBody EmployeeEntity employeeEntity) {
+    	return iEmployeeManager.updateEmployee(id, employeeEntity);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    EmployeeEntity deleteEmployee(@PathVariable Long id) {
+    	return null;
+    }
+    
 
 }
